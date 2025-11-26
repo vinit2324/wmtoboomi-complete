47d5ca7333a884b27b3b3a8d631e20968d6d736e
vinit2324 <vinitwip@gmail.com>
2025-11-26 11:02:32 -0800

    Initial commit: webMethods to Boomi Migration Accelerator v1.0

--- wmtoboomi-accelerator/backend/app/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-webMethods to Boomi Migration Accelerator - Backend Application
-
-Enterprise platform for automating 80-90% of webMethods to Boomi migrations.
-Built for Jade Global Inc.
-"""
-
-__version__ = "3.0.0"
-__author__ = "Jade Global Inc."
